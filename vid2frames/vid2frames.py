044ca98b722b53ebeff980c2b5ef531fca15e0c9
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2023-11-20 13:49:15 +0530

    vid2frames as function

--- vid2frames/vid2frames.py
+++ vid2frames/vid2frames.py
@@ -26,39 +26,40 @@ parser.add_argument('-imgf','--image_format',
                     help ='Path to save dir')
   
 args = parser.parse_args()
-  
-# Read the video from specified path
-cam = cv2.VideoCapture(args.source)
-  
-try:
-    # creating a folder named data
-    if not os.path.exists(args.destination):
-        os.makedirs(args.destination)
-  
-# if not created then raise error
-except OSError:
-    print ('Error: Creating directory of data')
-  
-# frame
-currentframe = 0
-  
-while(True):
-    # reading from frame
-    ret,frame = cam.read()
-  
-    if ret:
-        # if video is still left continue creating images
-        name = os.path.join(args.destination, str(currentframe) + '.' + args.image_format)
-        print ('Creating...' + name)
-  
-        # writing the extracted images
-        cv2.imwrite(name, frame)
-  
-        # increasing counter so that it will
-        # show how many frames are created
-        currentframe += 1
-    else:
-        break
-  
-# Release all space and windows once done
-cam.release()
+
+def vid2frames(source=args.source, destination=args.destination, img_format=args.image_format):
+    # Read the video from specified path
+    cam = cv2.VideoCapture(source)
+    
+    try:
+        # creating a folder named data
+        if not os.path.exists(destination):
+            os.makedirs(destination)
+    
+    # if not created then raise error
+    except OSError:
+        print ('Error: Creating directory of data')
+    
+    # frame
+    currentframe = 0
+    
+    while(True):
+        # reading from frame
+        ret,frame = cam.read()
+    
+        if ret:
+            # if video is still left continue creating images
+            name = os.path.join(destination, str(currentframe) + '.' + img_format)
+            print ('Creating...' + name)
+    
+            # writing the extracted images
+            cv2.imwrite(name, frame)
+    
+            # increasing counter so that it will
+            # show how many frames are created
+            currentframe += 1
+        else:
+            break
+    
+    # Release all space and windows once done
+    cam.release()
